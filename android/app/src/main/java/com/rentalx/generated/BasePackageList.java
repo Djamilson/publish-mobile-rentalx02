11204bd2dedc9f9cf267e774fe980825ad96b67d
Djamilson Alves da Costa <djamlson@gmail.com>
2021-09-13 15:04:39 -0300

    Update not codePush

--- android/app/src/main/java/com/rentalx/generated/BasePackageList.java
+++ android/app/src/main/java/com/rentalx/generated/BasePackageList.java
@@ -9,10 +9,12 @@ public class BasePackageList {
     return Arrays.<Package>asList(
         new expo.modules.application.ApplicationPackage(),
         new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.device.DevicePackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.updates.UpdatesPackage()
